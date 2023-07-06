de3d655f2ba4345aeb9a4371ed37a39e4fc0187c
Marco Giammari <marcogiammari@gmail.com>
2023-07-06 18:45:39 +0200

    change (store, migration) datatypes

--- database/migrations/2023_07_04_125211_create_comics_table.php
+++ database/migrations/2023_07_04_125211_create_comics_table.php
@@ -16,14 +16,14 @@ return new class extends Migration
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
             $table->string('title', 50);
-            $table->text('description');
-            $table->text('thumb');
-            $table->string('price', 10);
+            $table->text('description', 1000)->nullable();
+            $table->text('thumb', 1000)->nullable();
+            $table->float('price', 8, 2);
             $table->string('series', 50);
             $table->string('type', 50);
             $table->date('sale_date');
-            $table->json('artists');
-            $table->json('writers');
+            $table->json('artists')->nullable();
+            $table->json('writers')->nullable();
             $table->timestamps();
         });
     }
